48b2347417f99f3f7ba018e491a9797190e3a621
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-10 23:53:29 +0600

    finisher project

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -10,16 +10,17 @@ const Services = () => {
     },[])
     return (
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <h1 className="text-5xl font-bold text-center pb-2">Services</h1>
         <div className="grid gap-8 row-gap-5 mb-8 lg:grid-cols-3 lg:row-gap-8">
-            <h1 className='text-5xl font-bold text-center'>Services</h1>
-            {
-                services.map(service =><ServicesDetails
-                    key={service._id}
-                    service ={service}
-                ></ServicesDetails>)
-                // services.length
-            }
-         
+          {
+            services.map((service) => (
+              <ServicesDetails
+                key={service._id}
+                service={service}
+              ></ServicesDetails>
+            ))
+            // services.length
+          }
         </div>
       </div>
     );
