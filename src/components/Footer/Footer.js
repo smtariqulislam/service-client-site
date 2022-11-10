2d6c9da0cb86f8ea6b2b7801942f73817aa29af6
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-10 23:45:35 +0600

    footer last update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,23 +21,7 @@ const Footer = () => {
           </div>
           <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
            
-            <div className="space-y-3">
-              <h3 className="tracking-wide uppercase dark:text-gray-50">
-                Company
-              </h3>
-              <ul className="space-y-1">
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Privacy
-                  </a>
-                </li>
-                <li>
-                  <a rel="noopener noreferrer" href="#">
-                    Terms of Service
-                  </a>
-                </li>
-              </ul>
-            </div>
+            
             
             <div className="space-y-3">
               <div className="uppercase dark:text-gray-50">Social media</div>
